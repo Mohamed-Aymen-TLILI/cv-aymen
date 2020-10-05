d765ddc7ad09f92aa73ca7e13299074b257ca6cd
libra1919 <aymentli@gmail.com>
2020-10-05 22:17:44 +0200

    Updates

---
+++ precache-manifest.7414c7d74d8ac8da737fea9e1e4687d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a2768556e1b54192d791d4918f1956",
+    "url": "/cv-aymen/index.html"
+  },
+  {
+    "revision": "e285f732d093097014b7",
+    "url": "/cv-aymen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e2da3d5f5daa09c1314",
+    "url": "/cv-aymen/static/js/2.d67a4b70.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/cv-aymen/static/js/2.d67a4b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e285f732d093097014b7",
+    "url": "/cv-aymen/static/js/main.ba827ee8.chunk.js"
+  },
+  {
+    "revision": "a6b8448bb37e306d57da",
+    "url": "/cv-aymen/static/js/runtime-main.e85fbf6e.js"
+  },
+  {
+    "revision": "7a4e1c7710f639249292d4b13b83e413",
+    "url": "/cv-aymen/static/media/profile.7a4e1c77.jpg"
+  }
+]);
